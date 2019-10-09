c07b442cd5f81980d5bc59795b3bdbeed77385a1
Victor Pavlushin <victor@pavlushin.info>
2019-10-09 09:28:47 +0700

    Optymization

--- netbox_netdev_inventory/devices_list.py
+++ netbox_netdev_inventory/devices_list.py
@@ -37,29 +37,20 @@ def parse_filter_yaml_def(filter_yaml, creds=None):
     netbox_api = NetboxAPI(**get_config()["netbox"])
     devices = {}
     with open(filter_yaml) as filter_yaml_str:
-        q_list = []
         yml = yaml.safe_load(filter_yaml_str)
-        for fil, props in tqdm(yml["filter"].items()):
-            if props is not None:
-                if type(props) is list:
-                    q_list.append(fil + "=" +
-                                  ("&" + fil + "=").join(str(x) for x in props)
-                                  )
-                else:
-                    q_list.append(fil + "=" + str(props))
-        logger.info("Resultant filter %s", "&".join(q_list))
         platforms_js = netbox_api.get("dcim/platforms")
         platforms = {}
-        platforms_args = {}
         for platform in platforms_js["results"]:
             if platform["napalm_driver"]:
-                platforms[platform["id"]] = platform["napalm_driver"]
-                platforms_args[platform["id"]] = platform["napalm_args"]
-        if len(platforms) == 0:
-            logger.error("Not for one platform napalm_driver is not defined")
-            sys.exit(4)
+                platforms[platform["id"]] = {
+                    "napalm_driver": platform["napalm_driver"],
+                    "napalm_args":   platform["napalm_args"]
+                }
+        if not platforms:
+            raise Exception("Not for one platform napalm_driver is not "
+                            "defined")
 
-        devlist = netbox_api.get("dcim/devices/?" + "&".join(q_list))
+        devlist = netbox_api.get("dcim/devices/", params=yml["filter"].items())
         for device in devlist["results"]:
             if not device["platform"]:
                 continue
@@ -67,17 +58,21 @@ def parse_filter_yaml_def(filter_yaml, creds=None):
                 continue
 
             try:
+                if device["primary_ip"].get("address"):
+                    dev = device["primary_ip"].get("address").split("/")[0]
+                else:
+                    dev = device["name"]
                 devices[device["name"]] = DeviceImporter(
-                    device["primary_ip"].get("address").split("/")[0]
-                    or device["name"],
-                    napalm_driver_name=platforms[device["platform"]["id"]],
-                    napalm_optional_args=platforms_args[
+                    dev,
+                    napalm_driver_name=platforms[
+                         device["platform"]["id"]
+                    ]["napalm_driver"],
+                    napalm_optional_args=platforms[
                         device["platform"]["id"]
-                    ],
+                    ]["napalm_args"],
                     creds=creds,
                     discovery_protocol=yml["discovery_protocol"].get(
                         platforms[device["platform"]["id"]]
-
                     )
                 )
             except Exception as e:
